09042a729a3702b0dff6f6ece10856869d772bc7
Grace-Avor <graceavor2021@gmail.com>
2023-09-07 09:47:16 +0100

    final update

--- src/app.js
+++ src/app.js
@@ -1,43 +1,15 @@
-// Get the current date and time and update the "current-time" and "current-day" elements
 function updateDateTime() {
   const currentTime = document.getElementById("currentTime");
   const currentDay = document.getElementById("currentDay");
+
   const now = new Date();
-  const timeString = now.toISOString().split("T")[1].slice(0, 8); // Format the time as a string
-  const dayString = now.toLocaleDateString(undefined, { weekday: "long" }); // Get the full day name
-  currentTime.textContent = `Current Time: ${timeString}`;
-  currentDay.textContent = `Current Day: ${dayString}`;
+  const timeDisplay = now.toISOString().split("T")[1].slice(0, 8);
+  const dayDisplay = now.toLocaleDateString(undefined, { weekday: "long" });
+
+  currentTime.textContent = `Time: ${timeDisplay}`;
+  currentDay.textContent = `Day: ${dayDisplay}`;
 }
 
-// Update the date and time every second
 setInterval(updateDateTime, 1000);
 
-// Initial update
 updateDateTime();
-
-/*let now = new Date();
-let minutes = now.getUTCMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let hours = now.getUTCHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let day = now.getUTCDay();
-let days = [
-  "Friday",
-  "Saturday",
-  "Sunday",
-  "Monday",
-  "Thursday",
-  "Tuesday",
-  "Wednesday",
-];
-day = days[now.getUTCDay()];
-
-let time = document.querySelector("h3");
-time.innerHTML = `${hours}:${minutes}`;
-
-let weekDays = document.querySelector("h2");
-weekDays.innerHTML = `${day}`;*/
