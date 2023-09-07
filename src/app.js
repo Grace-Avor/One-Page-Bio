284ba4c6696584c6868d944acc47c2991c311c6f
Grace-Avor <graceavor2021@gmail.com>
2023-09-07 14:55:09 +0100

    re-arranged data-testid

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 function updateDateTime() {
   const currentTime = document.getElementById("currentTime");
-  const currentDay = document.getElementById("currentDay");
 
   const now = new Date();
   const timeDisplay = now.toISOString().split("T")[1].slice(0, 8);
-  const dayDisplay = now.toLocaleDateString(undefined, { weekday: "long" });
 
-  currentTime.textContent = `Time: ${timeDisplay}`;
-  currentDay.textContent = `Day: ${dayDisplay}`;
+  currentTime.textContent = `${timeDisplay}`;
 }
 
 setInterval(updateDateTime, 1000);
