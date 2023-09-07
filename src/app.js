5d73e060f484d027f006a0c38fc7a416e3f46ba2
Grace-Avor <graceavor2021@gmail.com>
2023-09-07 09:02:44 +0100

    changed time formaat properly

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
-let now = new Date();
+// Get the current date and time and update the "current-time" and "current-day" elements
+function updateDateTime() {
+  const currentTime = document.getElementById("currentTime");
+  const currentDay = document.getElementById("currentDay");
+  const now = new Date();
+  const timeString = now.toISOString().split("T")[1].slice(0, 8); // Format the time as a string
+  const dayString = now.toLocaleDateString(undefined, { weekday: "long" }); // Get the full day name
+  currentTime.textContent = `Current Time: ${timeString}`;
+  currentDay.textContent = `Current Day: ${dayString}`;
+}
+
+// Update the date and time every second
+setInterval(updateDateTime, 1000);
+
+// Initial update
+updateDateTime();
+
+/*let now = new Date();
 let minutes = now.getUTCMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
@@ -23,4 +40,4 @@ let time = document.querySelector("h3");
 time.innerHTML = `${hours}:${minutes}`;
 
 let weekDays = document.querySelector("h2");
-weekDays.innerHTML = `${day}`;
+weekDays.innerHTML = `${day}`;*/
